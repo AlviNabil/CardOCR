3fbae19f21865e2495123e6a48f0de9871b2ade7
billMac <alvi.nabil.aan@gmail.com>
2026-08-31 05:57:46 +0600

    Run build_runner for injectable dependency injection

--- card_ocr/lib/core/injection.config.dart
+++ card_ocr/lib/core/injection.config.dart
@@ -10,11 +10,15 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:card_ocr/core/di/register_module.dart' as _i1042;
+import 'package:card_ocr/data/data_sources/card_local_data_sources.dart'
+    as _i98;
 import 'package:card_ocr/data/data_sources/ocr_remote_data_sources.dart'
     as _i584;
+import 'package:card_ocr/data/repositories/card_repository_impl.dart' as _i506;
 import 'package:card_ocr/data/repositories/ocr_repository_impl.dart' as _i645;
 import 'package:card_ocr/data/services/crypto_services.dart' as _i37;
 import 'package:card_ocr/domain/domain.dart' as _i670;
+import 'package:card_ocr/domain/repositories/card_repository.dart' as _i554;
 import 'package:dio/dio.dart' as _i361;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart' as _i558;
 import 'package:get_it/get_it.dart' as _i174;
@@ -44,6 +48,21 @@ extension GetItInjectableX on _i174.GetIt {
         remoteDataSource: gh<_i584.OcrRemoteDataSource>(),
       ),
     );
+    gh.lazySingleton<_i37.CryptoServices>(
+      () =>
+          _i37.CryptoServices(secureStorage: gh<_i558.FlutterSecureStorage>()),
+    );
+    gh.lazySingleton<_i670.ScanCard>(
+      () => useCaseModule.scanCard(gh<_i670.OcrRepository>()),
+    );
+    gh.lazySingleton<_i98.CardLocalDataSource>(
+      () => _i98.CardLocalDataSource(cryptoService: gh<_i37.CryptoServices>()),
+    );
+    gh.lazySingleton<_i554.CardRepository>(
+      () => _i506.CardRepositoryImpl(
+        cardLocalDataSource: gh<_i98.CardLocalDataSource>(),
+      ),
+    );
     gh.lazySingleton<_i670.SaveCard>(
       () => useCaseModule.saveCard(gh<_i670.CardRepository>()),
     );
@@ -52,13 +71,6 @@ extension GetItInjectableX on _i174.GetIt {
     );
     gh.lazySingleton<_i670.DeleteCard>(
       () => useCaseModule.deleteCard(gh<_i670.CardRepository>()),
-    );
-    gh.lazySingleton<_i37.CryptoServices>(
-      () =>
-          _i37.CryptoServices(secureStorage: gh<_i558.FlutterSecureStorage>()),
-    );
-    gh.lazySingleton<_i670.ScanCard>(
-      () => useCaseModule.scanCard(gh<_i670.OcrRepository>()),
     );
     return this;
   }
